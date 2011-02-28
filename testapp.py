be925185dfb94efa14c2d0e589ebf03675dc8bb2
Thomas Pelletier <pelletier.thomas@gmail.com>
2011-02-28 12:08:06 +0100

    add jquery+ui

--- testapp.py
+++ testapp.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello World! foo"
+def upload():
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run()
